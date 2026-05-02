30099c395efd15a4b736056e16b101e127850669
Alan Machado <alanpinheirorj22@gmail.com>
2026-05-02 18:48:20 -0300

    feat: create list_task func

--- src/actions.rs
+++ src/actions.rs
@@ -1,24 +1,35 @@
 use std::io;
 
-struct Task{
-    id: String,
-    text: String,
-    status: Status
+#[derive(Debug)] 
+pub struct Task{
+    pub id: String,
+    pub text: String,
+    pub status: Status
 }
-enum Status{
+#[derive(Debug)]
+pub enum Status{
     Todo,
     Doing,
     Done
 }
-pub fn add_task(){
-    let mut tasks: Vec<Task> = Vec::new();
-    let mut ids = String::new();
+
+pub fn add_task(tasks: &mut Vec<Task>){
+    let mut id = String::new();
     let mut texts = String::new();
+    println!("enter a task name");
     io::stdin()
-        .read_line(&mut ids)
+        .read_line(&mut id)
         .expect("Error");
+    println!("enter a description");
     io::stdin()
         .read_line(&mut texts)
         .expect("Error");
-    tasks.push(Task { id: ids, text: texts, status: Status::Todo });
+    tasks.push(Task { 
+        id: id, text: texts, status: Status::Todo 
+    });
+}
+pub fn list_tasks(tasks: &Vec<Task>){
+    for task in tasks {
+        println!("Id:{}, Description{}, Status: {:?}", task.id, task.text, task.status);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use::std::io;
 mod actions;
-use actions::add_task;
+use actions::{add_task, Task};
 
 pub fn main(){
+    let mut tasks: Vec<Task> = Vec::new();
     let mut commands = String::new();
-    while true {
+    while 1 == 1 {
         io::stdin()
             .read_line(&mut commands)
             .expect("Error");
         if commands == "add"{
-            add_task();
+            add_task(&mut tasks);
         }
     }
    
